c0f2cb47582f7fa4aa4a7a94e4e4f4a7422a7d28
ramdhanyk <ram@novedea.com>
2014-07-09 11:56:10 -0400

    Update MortgageViewScript.sql

--- scripts/staging/MortgageViewScript.sql
+++ scripts/staging/MortgageViewScript.sql
@@ -3,7 +3,7 @@ SELECT a.*, cb.SSN as CBSSN, cb.[Borrower Email] as [CBorrower Email], cb.[Borro
 cb.[Borrower LastName] as [CBorrower LastName], cb.[Cell Phone] as [CBCell Phone], cb.City as CBCity,
 cb.[Current Street Address] as [CBCurrent Street Address], cb.[Date of Birth] as [CBDate of Birth],
 cb.Ethnicity as CBEthnicity, cb.[Home Phone] as [CBHome Phone], cb.[Marital Status] as [CBMarital Status],
-cb.Race as CBRace, cb.Sex as CBSex, cb.YearsAtThisAddress as CBYearsAtThisAddress, cb.Zip as CBZip, 
+cb.Race as CBRace, cb.Sex as CBSex, cb.YearsAtThisAddress as CBYearsAtThisAddress, cb.Zip as CBZip, cb.State as CBState,
 b.MonthlyIncome,b.Bonuses,b.Commission,b.OtherIncome,b.[Rent or Own],b.Checking,b.Savings,b.RetirementFund,b.MutualFund,
 c.[Property City],c.[Property State],c.[Property Usage],c.[Property Zip],c.Property_ID,c.RealEstateAgentEmail,c.RealEstateAgentName,c.RealEstateAgentPhone,
 d.Loan_ID,d.[Purpose of Loan],d.LoanAmount,d.[Purchase Price],d.CreditCardAuthorization,d.[Number of Units],d.Refferal,d.[Co-Borrower SSN]
